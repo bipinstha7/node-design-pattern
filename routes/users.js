756385c0b4bd38f77c9828915903f148198bb511
bipinstha7 <bipin.shrestha901@gmail.com>
2018-11-27 22:21:48 +0545

    new user route to get the data of currently logged in user's data

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,13 @@ const router = express.Router()
 const bcrypt = require('bcrypt')
 
 const { User, validate } = require('../models/user')
+const auth = require('../middleware/auth')
+
+router.get('/me', auth, async (req, res) => {
+	const user = await User.findById(req.user._id).select('-password')
+
+	res.send(user)
+})
 
 router.post('/', async (req, res) => {
 	const { error } = validate(req.body)
