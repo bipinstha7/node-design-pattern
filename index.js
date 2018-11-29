74fddc37518b4e8e3c8469adc5f0733987928a4a
bipinstha7 <bipin.shrestha901@gmail.com>
2018-11-29 22:46:45 +0545

    use auth router in index.js

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const customers = require('./routes/customers')
 const movie = require('./routes/movies')
 const rentals = require('./routes/rentals')
 const users = require('./routes/users')
+const auth = require('./routes/auth')
 
 const app = express()
 
@@ -24,6 +25,7 @@ app.use('/api/customers', customers)
 app.use('/api/movie', movie)
 app.use('/api/rentals', rentals)
 app.use('/api/users', users)
+app.use('/api/auth', auth)
 
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`Listening on port ${port}...`))
