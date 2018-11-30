e3975d9f23b49be6fcfc1f362bb360e4d55a3e35
bipinstha7 <bipin.shrestha901@gmail.com>
2018-11-30 22:45:52 +0545

    auth router to login

---
+++ routes/auth.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const router = express.Router()
+const bcrypt = require('bcrypt')
+
+const { User } = require('../models/user')
+
+router.post('/', async (req, res) => {
+	const { error } = validate(req.body)
+	if (error) return res.status(400).send(error.details[0].message)
+
+	try {
+		const { email, password } = req.body
+		let user = await User.findOne({ email })
+
+		if (!user) return res.status(400).send('Invalide email or password')
+
+		const validPassword = await bcrypt.compare(password, user.password)
+
+		if (!validPassword) return res.status(400).send('Invalide email or password')
+
+		res.send(true)
+	} catch (error) {
+		res.send(error)
+	}
+})
+
+function validate(req) {
+	const schema = {
+		email: Joi.string()
+			.min(5)
+			.max(255)
+			.required()
+			.email(),
+		password: Joi.string()
+			.min(5)
+			.max(255)
+			.required()
+	}
+
+	return Joi.validate(req, schema)
+}
+
+module.exports = router
