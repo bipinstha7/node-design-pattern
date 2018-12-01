d5266d9e818ff464dd11335de1b9a21a94193ffa
bipinstha7 <bipin.shrestha901@gmail.com>
2018-12-01 22:39:38 +0545

    hash user password before saving

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const bcrypt = require('bcrypt')
 
 const { User, validate } = require('../models/user')
 
@@ -8,22 +9,28 @@ router.post('/', async (req, res) => {
 	if (error) return res.status(400).send(error.details[0].message)
 
 	try {
-        let user = await User.findOne({ email: req.body.email })
-        
-        if (user) return res.status(400).send('User already registered.')
+		let user = await User.findOne({ email: req.body.email })
 
-		user = new User({ 
-            name: req.body.name,
-            email: req.body.email,
-            password: req.body.password
+		if (user) return res.status(400).send('User already registered.')
+
+		const { name, email, password } = req.body
+
+		user = new User({
+			name,
+			email,
+			password
         })
-
-        await user.save()
         
+        const salt = await bcrypt.genSalt(10)
+        user.password = await bcrypt.hash(user.password, salt)
+
+		await user.save()
+
 		res.send({
-            name: user.name,
-            email: user.email
-        })
+			_id: user._id,
+			name: user.name,
+			email: user.email
+		})
 	} catch (error) {
 		res.send(error)
 	}
