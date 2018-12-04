62be9cf7ab1516e0cffbb9420d8ae008854666c7
bipinstha7 <bipin.shrestha901@gmail.com>
2018-12-04 20:04:37 +0545

    move Customer schema to separate file and use it on router

--- routes/customers.js
+++ routes/customers.js
@@ -1,28 +1,7 @@
-const mongoose = require('mongoose')
-const Joi = require('joi')
 const express = require('express')
 const router = express.Router()
 
-const customerSchema = new mongoose.Schema({
-	name: {
-		type: String,
-		required: true,
-		minlength: 5,
-		maxlength: 50
-	},
-	isGold: {
-		type: Boolean,
-		default: false
-	},
-	phone: {
-		type: String,
-		required: true,
-		minlength: 5,
-		maxlength: 50
-	}
-})
-
-const Customer = mongoose.model('Customer', customerSchema)
+const { Customer, validate } = require('../models/customer')
 
 router.get('/', async (req, res) => {
 	try {
@@ -34,7 +13,7 @@ router.get('/', async (req, res) => {
 })
 
 router.post('/', async (req, res) => {
-	const { error } = validateCustomer(req.body)
+	const { error } = validate(req.body)
 	if (error) return res.status(400).send(error.details[0].message)
 
 	try {
@@ -51,7 +30,7 @@ router.post('/', async (req, res) => {
 })
 
 router.put('/:id', async (req, res) => {
-	const { error } = validateCustomer(req.body)
+	const { error } = validate(req.body)
 	if (error) return res.status(400).send(error.details[0].message)
 
 	try {
@@ -92,21 +71,5 @@ router.get('/:id', async (req, res) => {
 		res.send(error)
 	}
 })
-
-function validateCustomer(customer) {
-	const schema = {
-		name: Joi.string()
-			.min(5)
-			.max(50)
-			.required(),
-		phone: Joi.string()
-			.min(5)
-			.max(50)
-			.required(),
-		isGold: Joi.boolean()
-	}
-
-	return Joi.validate(customer, schema)
-}
 
 module.exports = router
