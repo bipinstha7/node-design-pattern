345505aa16423d425e4e54a043d9c9d9a78902b9
bipinstha7 <bipin.shrestha901@gmail.com>
2019-03-06 22:16:12 +0545

    new user router to create new user

---
+++ routes/users.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const router = express.Router()
+
+const { User, validate } = require('../models/user')
+
+router.post('/', async (req, res) => {
+	const { error } = validate(req.body)
+	if (error) return res.status(400).send(error.details[0].message)
+
+	try {
+        let user = await User.findOne({ email: req.body.email })
+        
+        if (user) return res.status(400).send('User already registered.')
+
+		user = new User({ 
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password
+        })
+
+        await user.save()
+        
+		res.send(user)
+	} catch (error) {
+		res.send(error)
+	}
+})
+
+module.exports = router
