5f32b7a4a09ab0299297830e74097390a95bb0bb
bipinstha7 <bipin.shrestha901@gmail.com>
2019-03-06 22:01:41 +0545

    create route optimization

--- routes/customers.js
+++ routes/customers.js
@@ -17,12 +17,12 @@ router.post('/', async (req, res) => {
 	if (error) return res.status(400).send(error.details[0].message)
 
 	try {
-		let customer = new Customer({
+		const customer = new Customer({
 			name: req.body.name,
 			phone: req.body.phone,
 			isGold: req.body.isGold
 		})
-		customer = await Customer.save()
+		await Customer.save()
 		res.send(customer)
 	} catch (error) {
 		res.send(error)
