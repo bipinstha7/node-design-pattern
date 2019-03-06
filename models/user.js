d92ad3b336baf8220db8c03fd3dad2289e6acca8
bipinstha7 <bipin.shrestha901@gmail.com>
2019-03-06 22:16:25 +0545

    new user model

---
+++ models/user.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose')
+const Joi = require('joi')
+
+const userSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: true,
+		minlength: 5,
+		maxlength: 50
+	},
+	email: {
+		type: String,
+		required: true,
+		minlength: 5,
+		maxlength: 255,
+		unique: true
+	},
+	password: {
+		type: String,
+		required: true,
+		minlength: 5,
+		maxlength: 1024
+	}
+})
+
+function validateUser(user) {
+	const schema = {
+		name: Joi.string()
+			.min(3)
+			.max(50)
+			.required(),
+		email: Joi.string()
+			.min(3)
+			.max(255)
+			.required()
+			.email(),
+		passsword: Joi.string()
+			.min(3)
+			.max(255) // user sends the plane text which can be hold by 255 character and that plane text will be hashed and store which can be hold by 1024
+			.required()
+	}
+
+	return Joi.validate(user, schema)
+}
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = {
+	User,
+	validate: validateUser
+}
