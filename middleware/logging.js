ef96a0d5738cec7be0376a7d5c23d6bc4b54ac9a
bipinstha7 <bipin.shrestha901@gmail.com>
2019-03-08 01:30:19 +0545

    logger file to log the errors

---
+++ middleware/logging.js
@@ -0,0 +1,8 @@
+const { createLogger, format, transports } = require('winston')
+const { combine, timestamp, prettyPrint } = format
+
+const logger = createLogger({
+	format: combine(timestamp(), prettyPrint()),
+	transports: [new transports.Console(), new transports.File({ filename: 'logger.log', level: 'error' })]
+})
+module.exports = logger
