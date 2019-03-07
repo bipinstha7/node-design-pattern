e398ceefa55a430c9b0726496ce48a8d58efe91a
bipinstha7 <bipin.shrestha901@gmail.com>
2019-03-08 01:36:17 +0545

    store logged data to mongodb database

--- middleware/logging.js
+++ middleware/logging.js
@@ -1,8 +1,22 @@
 const { createLogger, format, transports } = require('winston')
 const { combine, timestamp, prettyPrint } = format
+require('winston-mongodb')
+
+const keys = require('../config/keys')
 
 const logger = createLogger({
 	format: combine(timestamp(), prettyPrint()),
 	transports: [new transports.Console(), new transports.File({ filename: 'logger.log', level: 'error' })]
 })
+
+logger.add(
+	new transports.MongoDB({
+		db: keys.mongoURI,
+		// collection: 'logs',
+		// level: 'error',
+		// format: format.simple(),
+		// label: false
+	})
+)
+
 module.exports = logger
