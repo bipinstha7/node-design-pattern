d51a2dccadf9cc338c84bb7d0af299a33a0855c4
bipinstha7 <bipin.shrestha901@gmail.com>
2019-03-08 01:32:48 +0545

    log the error with level: 'error

--- middleware/error.js
+++ middleware/error.js
@@ -1,3 +1,10 @@
+const logger = require('./logging.js/index.js')
+
 module.exports = (err, req, res, next) => {
+	logger.log({
+		level: 'error',
+		message: err.message,
+		error: err
+	})
 	res.status(500).send('Something Failed.')
 }
