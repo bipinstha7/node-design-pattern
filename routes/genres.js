f3f9c623956126a7297be33795ac1bcac7761bbd
bipinstha7 <bipin.shrestha901@gmail.com>
2019-03-08 23:23:49 +0545

    use asyncMiddleware to handle async operations and remove try catch block from genres router

--- routes/genres.js
+++ routes/genres.js
@@ -4,16 +4,7 @@ const router = express.Router()
 const { Genre, validate } = require('../models/genre')
 const auth = require('../middleware/auth')
 const admin = require('../middleware/admin')
-
-function asyncMiddleware(handler) {
-	return async (req, res, next) => {
-		try {
-			await handler(req, res)
-		} catch (error) {
-			next(error)
-		}
-	}
-}
+const asyncMiddleware = require('../middleware/async')
 
 router.get(
 	'/',
@@ -23,56 +14,54 @@ router.get(
 	})
 )
 
-router.post('/', auth, async (req, res) => {
-	const { error } = validate(req.body)
-	if (error) return res.status(400).send(error.details[0].message)
+router.post(
+	'/',
+	auth,
+	asyncMiddleware(async (req, res) => {
+		const { error } = validate(req.body)
+		if (error) return res.status(400).send(error.details[0].message)
 
-	try {
 		const genre = new Genre({ name: req.body.name })
 		await genre.save()
 		res.send(genre)
-	} catch (error) {
-		res.status(500).send('Something Failed.')
-	}
-})
+	})
+)
 
-router.put('/:id', async (req, res) => {
-	const { error } = validate(req.body)
-	if (error) return res.status(400).send(error.details[0].message)
+router.put(
+	'/:id',
+	asyncMiddleware(async (req, res) => {
+		const { error } = validate(req.body)
+		if (error) return res.status(400).send(error.details[0].message)
 
-	try {
 		const genre = await Genre.findByIdAndUpdate(req.params.id, { name: req.body.name }, { new: true })
 
 		if (!genre) return res.status(404).send('The genre with the given ID was not found.')
 
 		res.send(genre)
-	} catch (error) {
-		res.status(500).send('Something Failed.')
-	}
-})
+	})
+)
 
-router.delete('/:id', [auth, admin], async (req, res) => {
-	try {
+router.delete(
+	'/:id',
+	[auth, admin],
+	asyncMiddleware(async (req, res) => {
 		const genre = await Genre.findByIdAndRemove(req.params.id)
 
 		if (!genre) return res.status(404).send('The genre with the given ID was not found.')
 
 		res.send(genre)
-	} catch (error) {
-		res.status(500).send('Something Failed.')
-	}
-})
+	})
+)
 
-router.get('/:id', async (req, res) => {
-	try {
+router.get(
+	'/:id',
+	asyncMiddleware(async (req, res) => {
 		const genre = await Genre.findById(req.params.id)
 
 		if (!genre) return res.status(404).send('The genre with the given ID was not found.')
 
 		res.send(genre)
-	} catch (error) {
-		res.status(500).send('Something Failed.')
-	}
-})
+	})
+)
 
 module.exports = router
