d7be11a08872cd90315c45ce3429c2d24353c322
bipinstha7 <bipin.shrestha901@gmail.com>
2019-03-09 10:25:41 +0545

    refactor routers to new route.js file and pass the app instance from index.js

--- index.js
+++ index.js
@@ -4,30 +4,18 @@ const Joi = require('joi')
 Joi.objectId = require('joi-objectid')(Joi)
 require('express-async-errors') // handles async operation with try-catch block without explicitely using try-catch block
 
-const genres = require('./routes/genres')
-const customers = require('./routes/customers')
-const movie = require('./routes/movies')
-const rentals = require('./routes/rentals')
-const users = require('./routes/users')
-const auth = require('./routes/auth')
 const keys = require('./config/keys')
-const error = require('./middleware/error')
 
 const app = express()
+
+require('./startup/routes')(app)
 
 mongoose
 	.connect(keys.mongoURI, { useNewUrlParser: true })
 	.then(() => console.log('Connected to MongoDB...'))
 	.catch(err => console.error('Could not connect to MongoDB...'))
 
-app.use(express.json())
-app.use('/api/genres', genres)
-app.use('/api/customers', customers)
-app.use('/api/movie', movie)
-app.use('/api/rentals', rentals)
-app.use('/api/users', users)
-app.use('/api/auth', auth)
-app.use(error)
+
 
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`Listening on port ${port}...`))
