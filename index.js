a49349624dfc68e6612715f8f12d58b6e90d78b0
bipinstha7 <bipin.shrestha901@gmail.com>
2019-03-09 10:34:07 +0545

    refactor mongodb connection

--- index.js
+++ index.js
@@ -1,21 +1,12 @@
 const express = require('express')
-const mongoose = require('mongoose')
 const Joi = require('joi')
 Joi.objectId = require('joi-objectid')(Joi)
 require('express-async-errors') // handles async operation with try-catch block without explicitely using try-catch block
 
-const keys = require('./config/keys')
-
 const app = express()
 
 require('./startup/routes')(app)
-
-mongoose
-	.connect(keys.mongoURI, { useNewUrlParser: true })
-	.then(() => console.log('Connected to MongoDB...'))
-	.catch(err => console.error('Could not connect to MongoDB...'))
-
-
+require('./startup/db')()
 
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`Listening on port ${port}...`))
