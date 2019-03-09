a5fb0fd72ed5b0eda26fb7417af8626c695515a2
bipinstha7 <bipin.shrestha901@gmail.com>
2019-03-09 10:37:51 +0545

    require validation module

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require('express')
-const Joi = require('joi')
-Joi.objectId = require('joi-objectid')(Joi)
 require('express-async-errors') // handles async operation with try-catch block without explicitely using try-catch block
 
 const app = express()
 
 require('./startup/routes')(app)
 require('./startup/db')()
+require('./startup/validation')
 
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`Listening on port ${port}...`))
