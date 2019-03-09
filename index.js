bd9b311ef39f3d2fe2cc3d08f17636962067c495
bipinstha7 <bipin.shrestha901@gmail.com>
2019-03-09 10:39:31 +0545

    log server listen with winston

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ require('express-async-errors') // handles async operation with try-catch block 
 
 const app = express()
 
+const logger = require('./middleware/logging')
 require('./startup/routes')(app)
 require('./startup/db')()
 require('./startup/validation')
 
 const port = process.env.PORT || 3000
-app.listen(port, () => console.log(`Listening on port ${port}...`))
+app.listen(port, () => logger.info(`Listening on port ${port}...`))
