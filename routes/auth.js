ec14c48a47be66e0c615f2ff8e3acac2223301d7
bipinstha7 <bipin.shrestha901@gmail.com>
2019-05-14 07:37:44 +0545

    fix invalid typo

--- routes/auth.js
+++ routes/auth.js
@@ -12,11 +12,11 @@ router.post('/', async (req, res) => {
 	const { email, password } = req.body
 	let user = await User.findOne({ email })
 
-	if (!user) return res.status(400).send('Invalide email or password')
+	if (!user) return res.status(400).send('Invalid email or password')
 
 	const validPassword = await bcrypt.compare(password, user.password)
 
-	if (!validPassword) return res.status(400).send('Invalide email or password')
+	if (!validPassword) return res.status(400).send('Invalid email or password')
 
 	// generateAuthToken method has been created at user model
 	const token = user.generateAuthToken()
