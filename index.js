fdd5cca0ae23fd3754831d82dfded9bb7eb5a8c3
bipinstha7 <bipin.shrestha901@gmail.com>
2019-07-25 23:55:52 +0545

    Use cors

--- index.js
+++ index.js
@@ -8,11 +8,19 @@ require('express-async-errors')
 
 const app = express()
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+    
+    next()
+})
+
 const logger = require('./middleware/logging')
 require('./startup/routes')(app)
 require('./startup/db')()
 require('./startup/validation')
 require('./startup/prod')(app)
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 4000
 app.listen(port, () => logger.info(`Listening on port ${port}...`))
