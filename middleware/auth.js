8ec828e9c8c8dbd65de8114cb6e2ce5fba024dd0
bipinstha7 <bipin.shrestha901@gmail.com>
2019-07-25 23:45:26 +0545

    Bypass auth to test the react project

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,6 +2,8 @@ const jwt = require('jsonwebtoken')
 const keys = require('../config/keys')
 
 function auth(req, res, next) {
+	if (!keys.requiresAuth) return next();
+
 	const token = req.header('x-auth-token')
 	if (!token) return res.status(401).send('Access denied. No token provided.')
 
